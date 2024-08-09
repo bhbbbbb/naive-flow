b7c244eb5291b0b6ded072b78245a2a492b595cf
Smark Lin <r12942082@ntu.edu.tw>
2024-08-09 15:08:01 +0800

    fix loading bug of load_checkpoint for new ckpt format

--- naive_flow/__init__.py
+++ naive_flow/__init__.py
@@ -7,16 +7,25 @@ def load_checkpoint(checkpoint_path: str):
     # pylint: disable=import-outside-toplevel
     """Load the specific checkpoint save by Tracker"""
     import os
+    import glob
     import torch
     from typing import get_type_hints
     from .tracker.base_tracker import _CheckpointDict
 
-    checkpoint_dict: _CheckpointDict = torch.load(checkpoint_path)
+    if os.path.isfile(checkpoint_path):
+        # backward compatibility
+        checkpoint_dict: _CheckpointDict = torch.load(checkpoint_path)
 
-    if set(get_type_hints(_CheckpointDict).keys()
-           ) <= set(checkpoint_dict.keys()):
-        return checkpoint_dict["user_data"]
-    checkpoint_name = os.path.basename(checkpoint_path)
-    raise ValueError(
-        f"Checkpoint: {checkpoint_name} is not a checkpoint stored by Tracker."
-    )
+        if set(get_type_hints(_CheckpointDict).keys()
+               ) <= set(checkpoint_dict.keys()):
+            return checkpoint_dict["user_data"]
+        checkpoint_name = os.path.basename(checkpoint_path)
+        raise ValueError(
+            f"Checkpoint: {checkpoint_name} is not a checkpoint stored by Tracker."
+        )
+    else:
+        user_data = {}
+        for path in glob.glob(os.path.join(checkpoint_path, "*.pth")):
+            name, _ = os.path.splitext(os.path.basename(path))
+            user_data[name] = torch.load(path)
+        return user_data
